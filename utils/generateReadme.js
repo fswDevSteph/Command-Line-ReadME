d89a70216b52bbf5c80f6ddafdc2f8ed4e740758
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-01-23 20:57:38 -0400

    toc

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -9,11 +9,15 @@ export function generateReadMe(answers) {
 ## License 
 ${answers.license}
 
+## Table of Contents
+${answers.toc}
+
 ## Installation
     npm install ${answers.title.toLowerCase().replaceAll(' ', '-')}
     
 ## Questions
 ${answers.questions}
+
 For more information contact me here:
 GitHub link: (https://github.com/${answers.questions})
 
